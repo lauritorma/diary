1b5770b6d8cd0201dc341b2d371470cc343117c6
Lauri Törmä <90974678+lauritorma@users.noreply.github.com>
2022-12-13 22:08:18 +0200

    debug

--- src/main/java/com/example/Diary/domain/EntryRepository.java
+++ src/main/java/com/example/Diary/domain/EntryRepository.java
@@ -5,6 +5,5 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface EntryRepository extends CrudRepository<Entry, Long> {
-
-	List<Entry> findByEntryname(String entryname);
+	
 }
